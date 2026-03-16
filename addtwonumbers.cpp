3e4317d96aac5fb538c12d14eca870b56842b08d
たき <prithvirajannk@gmail.com>
2026-03-16 16:11:00 +0530

    Add function to sum two linked list numbers
    Implement a function to add two numbers represented by linked lists.

---
+++ addtwonumbers.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
+        ListNode* dummy = new ListNode(0);
+        ListNode* current = dummy;
+        int carry = 0;
+        while (l1 != nullptr || l2 != nullptr || carry){
+            int val1 = (l1 != nullptr) ? l1->val : 0;
+            int val2 = (l2 != nullptr) ? l2->val : 0;
+            int sum = val1 + val2 + carry;
+            carry = sum / 10;
+            int digit = sum % 10;
+            current->next = new ListNode(digit);
+            current = current->next;
+            l1 = (l1 != nullptr) ? l1->next : nullptr;
+            l2 = (l2 != nullptr) ? l2->next : nullptr;
+        }
+        return dummy->next;
+        
+    }
+};
